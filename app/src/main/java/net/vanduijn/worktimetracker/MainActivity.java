8fdbb7703f0362b872c0635a3e8c2cf296fafa1f
Svenyd <sven@vanduijn.net>
2020-03-25 15:53:53 +0100

    Added save start work to FireBase

--- app/src/main/java/net/vanduijn/worktimetracker/MainActivity.java
+++ app/src/main/java/net/vanduijn/worktimetracker/MainActivity.java
@@ -1,27 +1,41 @@
 package net.vanduijn.worktimetracker;
 
-import androidx.annotation.RequiresApi;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MAIN_ACTIVITY";
     private boolean isWorking = false;
+    private String logRef;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
     public void startStopWorking(View v) {
         LocalDateTime now = LocalDateTime.now();
 
@@ -32,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
             Log.d("StartTime", now.toString());
         } else {
             saveEndTime(now);
+            this.isWorking = false;
+            updateStartStopButton();
             Log.d("EndTime", now.toString());
         }
     }
@@ -47,6 +63,26 @@ public class MainActivity extends AppCompatActivity {
 
     private void saveStartTime(LocalDateTime now) {
         //TODO: save startTime to Firebase
+        Map<String, String> log = new HashMap<>();
+        log.put("start_time", now.toString());
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        assert user != null;
+        db.collection(user.getUid()).document("work_log").collection(now.toString().substring(0, 7))
+                .add(log)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        logRef = documentReference.getId();
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
     }
 
     private void saveEndTime(LocalDateTime now) {
