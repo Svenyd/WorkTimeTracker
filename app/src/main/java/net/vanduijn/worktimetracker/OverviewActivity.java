6e4a20fa405ee3a4035882a0fc01d56daef1f4de
Svenyd <sven@vanduijn.net>
2020-03-30 21:01:42 +0200

    Added change month

--- app/src/main/java/net/vanduijn/worktimetracker/OverviewActivity.java
+++ app/src/main/java/net/vanduijn/worktimetracker/OverviewActivity.java
@@ -7,7 +7,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.time.LocalDateTime;
+
 public class OverviewActivity extends AppCompatActivity {
+
+    private LocalDateTime dateTime = LocalDateTime.now().withDayOfMonth(1).withHour(0).withMinute(0).withSecond(0).withNano(0);
+
+    private TextView txtMonth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,20 +25,25 @@ public class OverviewActivity extends AppCompatActivity {
         Button btnPrevious = findViewById(R.id.btn_previous_month);
         btnPrevious.setWidth(getResources().getDisplayMetrics().widthPixels/3);
 
-        TextView txtMonth = findViewById(R.id.txt_month);
+        txtMonth = findViewById(R.id.txt_month);
         txtMonth.setWidth(getResources().getDisplayMetrics().widthPixels/3);
+        txtMonth.setText(getMonthYearString(dateTime));
 
         Button btnNext = findViewById(R.id.btn_next_month);
         btnNext.setWidth(getResources().getDisplayMetrics().widthPixels/3);
-
-
     }
 
     public void previousMonth(View v) {
-
+        dateTime = dateTime.minusMonths(1);
+        txtMonth.setText(getMonthYearString(dateTime));
     }
 
     public void nextMonth(View v) {
+        dateTime = dateTime.plusMonths(1);
+        txtMonth.setText(getMonthYearString(dateTime));
+    }
 
+    private String getMonthYearString(LocalDateTime dateTime) {
+        return dateTime.getMonth().toString() + " " + dateTime.getYear();
     }
 }
